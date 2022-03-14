14acb5624aec22230198687a867c604b1b996efa
rostekus <rostyslavmosorov@gmail.com>
2022-03-15 00:11:43 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='Document Scanner',
+    version='1.0.0',
+    description='ocr document scannner',
+    author='Rostyslav Mosorov',
+    author_email='rmosorov@icloud.com',
+    license='MIT License',
+    url='https://github.com/rostekus/Document-Scanner',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
+
+)
